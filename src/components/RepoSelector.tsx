04513e9768b1ef9b4b1c234cd9cf3ce8ec627f7d
Adam Hall <adamhalldesigns@gmail.com>
2019-08-18 15:54:55 +0100

    change from tslint to eslint

--- src/components/RepoSelector.tsx
+++ src/components/RepoSelector.tsx
@@ -1,50 +1,68 @@
-import React, { useEffect, useState } from "react";
-import { IPrDataRequest } from "../types";
-import { getRepos, putRepos } from "../utils/fetchWrappers";
+import React, { useEffect, useState } from 'react';
+import { IPrDataRequest } from '../types';
+import { getRepos, putRepos } from '../utils/fetchWrappers';
 
 const initialAvailableRepos: IPrDataRequest[] = [];
 export const RepoInfo: React.FC<{
-    subscribedRepos: string[],
-    setSubscribedRepos: (data: string[]) => void,
+    subscribedRepos: string[];
+    setSubscribedRepos: (data: string[]) => void;
 }> = ({ setSubscribedRepos, subscribedRepos }) => {
-    useEffect(() => getRepos(setAvailableRepos), []);
     const [availableRepos, setAvailableRepos] = useState(initialAvailableRepos);
+    useEffect(() => getRepos(setAvailableRepos), []);
 
     const newRepo: IPrDataRequest = {
-        name: "",
-        owner: "",
+        name: '',
+        owner: '',
         prCount: 1,
         reviewsCount: 1,
     };
 
     return (
         <>
-            {availableRepos.map((repo) => (
-                <div>
+            {availableRepos.map(repo => (
+                <div key={repo.name}>
                     <input type="checkbox" value={repo.name} onClick={subscribeToRepo} />
                     <p>
                         {repo.name}: {repo.owner}
                     </p>
                 </div>
             ))}
-            <button onClick={() => { putRepos([newRepo], setAvailableRepos); }}>Add Repo</button>
-            <button onClick={() => { getRepos(setAvailableRepos); }}>Refresh RepoInfo</button>
+            <button
+                onClick={() => {
+                    putRepos([newRepo], setAvailableRepos);
+                }}
+            >
+                Add Repo
+            </button>
+            <button
+                onClick={() => {
+                    getRepos(setAvailableRepos);
+                }}
+            >
+                Refresh RepoInfo
+            </button>
         </>
     );
 
-    function subscribeToRepo(e: React.MouseEvent<HTMLInputElement>) {
+    function subscribeToRepo(e: React.MouseEvent<HTMLInputElement>): void {
         const { value, checked } = e.currentTarget;
         if (checked) {
-            setSubscribedRepos(dedupeA(subscribedRepos.concat(e.currentTarget.value)));
+            const repos = dedupeA(subscribedRepos.concat(e.currentTarget.value));
+            window.localStorage.setItem('subscribedRepos', JSON.stringify(repos));
+            setSubscribedRepos(repos);
         } else {
-            setSubscribedRepos(dedupeA(subscribedRepos).filter((repo) => repo !== value));
+            setSubscribedRepos(dedupeA(subscribedRepos).filter(repo => repo !== value));
         }
     }
-
 };
 
 function dedupeA<T>(arr: T[]): T[] {
     return arr.reduce(dedupe, []);
 
-    function dedupe<TT>(all: TT[], cur: any): TT[] { if (all.includes(cur)) { return all; } return all.concat(cur); }
+    function dedupe<TT>(all: TT[], cur: any): TT[] {
+        if (all.includes(cur)) {
+            return all;
+        }
+        return all.concat(cur);
+    }
 }
--- src/utils/fetchWrappers.ts
+++ src/utils/fetchWrappers.ts
@@ -1,64 +1,61 @@
-import { constants } from "../constants";
-import { IPrData, IPrDataRequest, isPrData, isPrDataRequest } from "../types";
+import { constants } from '../constants';
+import { IPrData, IPrDataRequest, isPrData, isPrDataRequest } from '../types';
 
 export function getPullRequests(repoData: IPrDataRequest, storePullRequests: (data: IPrData) => void) {
     fetch(`${constants.baseURL}/reviews`, {
         body: JSON.stringify(repoData),
-        headers: { "Content-Type": "application/json" },
-        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
     })
-        .then((res) => {
-            res
-                .json()
-                .then((data) => {
+        .then(res => {
+            res.json()
+                .then(data => {
                     if (isPrData(data)) {
                         storePullRequests(data);
                     } else {
-                        console.warn("reviews.request.not.of.type.IPrData", data);
+                        console.warn('reviews.request.not.of.type.IPrData', data);
                     }
                 })
-                .catch((e) => console.error("json error", e));
+                .catch(e => console.error('json error', e));
         })
-        .catch((e) => console.error("fetch error", e));
+        .catch(e => console.error('fetch error', e));
 }
 
 export function getRepos(storeRepos: (data: IPrDataRequest[]) => void) {
     fetch(`${constants.baseURL}/repos`, {
-        headers: { "Content-Type": "application/json" },
-        method: "GET",
+        headers: { 'Content-Type': 'application/json' },
+        method: 'GET',
     })
-        .then((res) => {
-            res
-                .json()
+        .then(res => {
+            res.json()
                 .then((data: IPrDataRequest[]) => {
                     if (data.every(isPrDataRequest)) {
                         storeRepos(data);
                     } else {
-                        console.warn("repos.request.not.of.type.Repos", data);
+                        console.warn('repos.request.not.of.type.Repos', data);
                     }
                 })
-                .catch((e) => console.error("json error", e));
+                .catch(e => console.error('json error', e));
         })
-        .catch((e) => console.error("fetch error", e));
+        .catch(e => console.error('fetch error', e));
 }
 
 export function putRepos(repos: IPrDataRequest[], storeRepos: (data: IPrDataRequest[]) => void) {
     fetch(`${constants.baseURL}/repos`, {
         body: JSON.stringify(repos),
-        headers: { "Content-Type": "application/json" },
-        method: "PUT",
+        headers: { 'Content-Type': 'application/json' },
+        method: 'PUT',
     })
-        .then((res) => {
-            res
-                .json()
+        .then(res => {
+            res.json()
                 .then((data: IPrDataRequest[]) => {
                     if (data.every(isPrDataRequest)) {
                         storeRepos(data);
                     } else {
-                        console.warn("repos.request.not.of.type.Repos", data);
+                        console.warn('repos.request.not.of.type.Repos', data);
                     }
                 })
-                .catch((e) => console.error("json error", e));
+                .catch(e => console.error('json error', e));
         })
-        .catch((e) => console.error("fetch error", e));
+        .catch(e => console.error('fetch error', e));
 }
